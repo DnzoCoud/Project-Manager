0f6b125f6874a82af527d66f56c9bd20b0521fcf
Daniel Rodriguez <dangelsfocus@gmail.com>
2025-01-24 14:58:36 -0500

    fix: enabled cors in gateaway

--- project-manager/apps/project-manager-api-gateaway/src/main.ts
+++ project-manager/apps/project-manager-api-gateaway/src/main.ts
@@ -5,6 +5,7 @@ import { MICROSERVICE_PORTS } from '@app/contracts/microservices-ports';
 async function bootstrap() {
   const app = await NestFactory.create(ProjectManagerApiGateawayModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
   await app.listen(process.env.port ?? MICROSERVICE_PORTS.API_GATEAWAY);
 }
 bootstrap();
